6d4203441ed4a9c262c5d5d0bedbf7645e97abc0
Bryan De La Cruz <bryan.delacruza@gmail.com>
2023-03-14 22:14:11 -0500

    exact path added

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,12 @@ function App() {
 
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Login />} />
+      <Route exact path="login" element={<Login />} />
+      <Route exact path="register" element={<Login />} />
       <Route element={<MenuLayout />}>
-        <Route path="home" element={<Home />} />
+        <Route exact path="home" element={<Home />} />
       </Route>
-      <Route path="*" element={<Login />}></Route>
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
